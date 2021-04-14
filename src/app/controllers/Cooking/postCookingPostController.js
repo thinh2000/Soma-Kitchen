f1fa4ca19dce4378c433a0bb148ec27d0b4778a2
ducnguyen2000 <only4wns@gmail.com>
2021-04-14 11:56:39 +0700

    FIX: Fix minor bugs in upload files

--- src/app/controllers/Cooking/postCookingPostController.js
+++ src/app/controllers/Cooking/postCookingPostController.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = (req, res) => {
     let video = req.files.video;
-    video.mv(path.resolve(__dirname, '../../../', '/upload/videos', video.name),
+    video.mv(path.resolve(__dirname, '../../../', '/upload/videos/', video.name),
     (err) => {
         CookingPost.create({
             ...req.body,
--- src/app/controllers/Recipe/postRecipePostController.js
+++ src/app/controllers/Recipe/postRecipePostController.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = (req, res) => {
     let image = req.files.image;
-    image.mv(path.resolve(__dirname, '../../../', '/upload/images', image.name),
+    image.mv(path.resolve(__dirname, '../../../', '/upload/images/', image.name),
     (err) => {
         RecipePost.create({
             ...req.body,
